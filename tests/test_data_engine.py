bf2ffc1ced646407dc8ce18a62b7d85926af1f94
amin-alavian <112092631+m-amin-alavian@users.noreply.github.com>
2023-03-25 22:08:34 +0430

    Add variable names to function input

--- tests/test_data_engine.py
+++ tests/test_data_engine.py
@@ -3,4 +3,4 @@ from hbsir.data_engine import load_table, add_attribute
 df = load_table('food', 1400)
 
 def test_add_attribute():
-    assert len(add_attribute(df, 1400, ["Region"]).columns) == len(df.columns) + 1
+    assert len(add_attribute(df, year=1400, attribute=["Region"]).columns) == len(df.columns) + 1
