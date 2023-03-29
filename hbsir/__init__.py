f0c22a8b3defb98f14c0075fc42ac52c32ea1f12
amin-alavian <112092631+m-amin-alavian@users.noreply.github.com>
2023-03-30 03:02:44 +0430

    Add new classes and functions to init

--- hbsir/__init__.py
+++ hbsir/__init__.py
@@ -1,7 +1,12 @@
 """init file"""
 
+from .hbsframe import HBSDF
+
 from .data_engine import (
     load_table,
+    read_hbs,
     add_attribute,
     add_classification,
 )
+
+__version__ = "0.1.0"
