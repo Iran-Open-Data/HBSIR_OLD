1d84198011532d11a3ba67148a9022d60d710cb6
amin-alavian <112092631+m-amin-alavian@users.noreply.github.com>
2023-04-03 05:26:10 +0430

    Update maintainer

--- hbsir/maintainer.py
+++ hbsir/maintainer.py
@@ -2,6 +2,7 @@
 
 import tomllib
 
+import requests
 from tqdm import tqdm
 import boto3
 
@@ -12,7 +13,68 @@ from .metadata import (
 from . import utils
 
 
-def upload_file_to_online_directory(file_path, file_name):
+def update_online_directory() -> None:
+    file_stats = _scan_local_directory()
+    to_up_files = [file for file, stat in file_stats.items() if stat != "UpToDate"]
+    pbar = tqdm(total=len(to_up_files), desc="Preparing ...", unit="Table")
+    for file in to_up_files:
+        pbar.update()
+        pbar.desc = f"Checking file: {file}"
+        _publish_parquet_file(file)
+    pbar.close()
+
+
+def _scan_local_directory():
+    file_stats = {}
+    number_of_files = len(list(defaults.processed_data.glob("*")))
+    pbar = tqdm(total=number_of_files, desc="Preparing ...", unit="Table")
+    for path in defaults.processed_data.iterdir():
+        pbar.update()
+        pbar.desc = f"Checking file: {path.name}"
+        online_file_size = _get_file_size_online_directory(path.name)
+        local_size = path.stat().st_size
+
+        if online_file_size is None:
+            file_stats[path.name] = "Missing"
+        elif abs(online_file_size - local_size) < 5:
+            file_stats[path.name] = "UpToDate"
+        else:
+            file_stats[path.name] = "OutDated"
+    pbar.close()
+    return file_stats
+
+
+def _get_file_size_online_directory(file_name):
+    url = f"{defaults.online_dir}/parquet_files/{file_name}"
+    response = requests.head(url, timeout=10)
+    try:
+        return int(response.headers["Content-Length"])
+    except KeyError:
+        return None
+
+
+def publish_data(
+    table_name: _OriginalTable | list[_OriginalTable] | None = None,
+    from_year: int | None = None,
+    to_year: int | None = None,
+) -> None:
+    table_year = utils.create_table_year_product(table_name, from_year, to_year)
+    pbar = tqdm(total=len(table_year), desc="Preparing ...", unit="Table")
+    for _table_name, year in table_year:
+        pbar.update()
+        pbar.desc = f"Uploading table: {_table_name}, for year: {year}"
+        file_name = f"{year}_{_table_name}.parquet"
+        _publish_parquet_file(file_name)
+    pbar.close()
+
+
+def _publish_parquet_file(file_name: str) -> None:
+    path = defaults.processed_data.joinpath(file_name)
+    url = f"HBSIR/parquet_files/{file_name}"
+    _upload_file_to_online_directory(path, url)
+
+
+def _upload_file_to_online_directory(file_path, file_name):
     bucket = _get_bucket()
 
     with open(file_path, "rb") as file:
@@ -21,6 +83,7 @@ def upload_file_to_online_directory(file_path, file_name):
             Body=file,
             Key=file_name
         )
+
 
 def _get_bucket(bucket_name='sdac'):
     with open("tokens.toml", "rb") as f:
@@ -35,18 +98,3 @@ def _get_bucket(bucket_name='sdac'):
     return bucket
 
 
-def publish_parquet(
-    table_name: _OriginalTable | list[_OriginalTable] | None = None,
-    from_year: int | None = None,
-    to_year: int | None = None,
-) -> None:
-    table_year = utils.create_table_year_product(table_name, from_year, to_year)
-    pbar = tqdm(total=len(table_year), desc="Preparing ...", unit="Table")
-    for _table_name, year in table_year:
-        pbar.update()
-        pbar.desc = f"Uploading table: {_table_name}, for year: {year}"
-        file_name = f"{year}_{_table_name}.parquet"
-        path = defaults.processed_data.joinpath(file_name)
-        url = f"HBSIR/parquet_files/{file_name}"
-        upload_file_to_online_directory(path, url)
-    pbar.close()
