d30b3450d2686cd9e99b56114199346e1bffbb6c
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-08-02 02:24:20 +0330

    Add load_table function

--- hbsir/__init__.py
+++ hbsir/__init__.py
@@ -1,3 +1,21 @@
 """init file"""
 
+from typing import Iterable
+
+import pandas as pd
+
+from .data_engine import TableLoader
+
+def load_table(
+    table_names: str | list[str],
+    years: int | Iterable[int] | str | None,
+    apply_schema: bool = True,
+) -> pd.DataFrame:
+    loader = TableLoader(
+        table_names=table_names,
+        years=years,
+        apply_schema=apply_schema,
+    )
+    return loader.load()
+
 __version__ = "0.1.0"
