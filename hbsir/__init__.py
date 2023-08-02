201e192f96a8105b3dc084db36224a71b50c0b7b
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-08-02 20:32:03 +0330

    Add add_classification to public api

--- hbsir/__init__.py
+++ hbsir/__init__.py
@@ -4,18 +4,28 @@ from typing import Iterable
 
 import pandas as pd
 
-from .data_engine import TableLoader
+from . import data_engine
+
 
 def load_table(
     table_names: str | list[str],
     years: int | Iterable[int] | str | None,
     apply_schema: bool = True,
 ) -> pd.DataFrame:
-    loader = TableLoader(
+    loader = data_engine.TableLoader(
         table_names=table_names,
         years=years,
         apply_schema=apply_schema,
     )
     return loader.load()
+
+
+def add_classification(
+    table: pd.DataFrame, classification_name: str, **kwargs
+) -> pd.DataFrame:
+    table = data_engine.add_classification(
+        table=table, classification_name=classification_name, **kwargs
+    )
+    return table
 
 __version__ = "0.1.0"
