7b73a825bc603e4e843a0d2afea495eb7866d340
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-08-07 02:50:41 +0330

    Add load table settings

--- hbsir/__init__.py
+++ hbsir/__init__.py
@@ -1,27 +1,65 @@
 """init file"""
 
-from typing import Iterable
+from typing import Iterable, Literal
 
 import pandas as pd
 
-from . import data_engine
+from . import data_engine, metadata, data_cleaner, utils
 
 
+# pylint: disable=unused-argument
 def load_table(
-    table_names: str | list[str],
+    table_name: str,
     years: int | Iterable[int] | str | None,
-    apply_schema: bool = True,
+    data_type: Literal["processed", "cleaned", "original"] | None = None,
+    on_missing: Literal["error", "download", "create"] | None = None,
+    save_downloaded: bool | None = None,
+    save_created: bool | None = None,
 ) -> pd.DataFrame:
-    loader = data_engine.TableLoader(
-        table_names=table_names,
-        years=years,
-        apply_schema=apply_schema,
-    )
-    return loader.load()
+    metadata.metadatas.reload_schema()
+    optional_vars = {key: value for key, value in locals().items() if value is not None}
+    settings = metadata.LoadTable(**optional_vars)
+    if settings.data_type == "original":
+        years = utils.parse_years(years)
+        table_parts = []
+        for year in years:
+            table_parts.append(data_cleaner.read_table_csv(table_name, year))
+        table = pd.concat(table_parts)
+    elif settings.data_type == "cleaned":
+        years = utils.parse_years(years)
+        table_parts = []
+        for year in years:
+            table_parts.append(
+                data_engine.TableHandler(table_name, year, settings).read()
+            )
+        table = pd.concat(table_parts)
+    else:
+        loader = data_engine.TableLoader(
+            table_name=table_name,
+            years=years,
+            settings=settings,
+        )
+        table = loader.load()
+    return table
 
 
-def create_table_with_schema(schema) -> pd.DataFrame:
-    return data_engine.TableLoader(schema=schema).load()
+# pylint: disable=unused-argument
+def create_table_with_schema(
+    schema: dict,
+    data_type: Literal["processed", "cleaned", "original"] | None = None,
+    on_missing: Literal["error", "download", "create"] | None = None,
+    save_downloaded: bool | None = None,
+    save_created: bool | None = None,
+) -> pd.DataFrame:
+    metadata.metadatas.reload_schema()
+    optional_vars = {key: value for key, value in locals().items() if value is not None}
+    settings = metadata.LoadTable(**optional_vars)
+    if "table_list" in schema:
+        years = schema["years"]
+        metadata.metadatas.schema["_Input_Table"] = schema
+    else:
+        raise NameError
+    return data_engine.TableLoader("_Input_Table", years, settings).load()
 
 
 def add_classification(
