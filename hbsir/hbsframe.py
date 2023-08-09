2a0b1bbdc6c59ae01bdc9525efe1f03bec5a4f61
Mohammad Amin Alavian <112092631+m-amin-alavian@users.noreply.github.com>
2023-08-09 17:54:12 +0330

    Refactor classification (#40)

--- hbsir/hbsframe.py
+++ hbsir/hbsframe.py
@@ -3,10 +3,8 @@ DataFrame extention
 """
 import pandas as pd
 
-from .data_engine import add_attribute, add_classification
-from .data_engine import (
-    Attribute as _Attributes,
-)
+from .metadata_reader import Attribute as _Attributes
+from . import decoder
 
 
 @pd.api.extensions.register_dataframe_accessor("view")
@@ -30,49 +28,6 @@ class ViewAccessor:
 
     def __getitem__(self, value: str) -> pd.DataFrame:
         if self._views is not None and value in self._views:
-            return add_classification(self._obj, classification_name=value)
-        else:
-            raise KeyError(f"{value} is not a view of the current table")
-
-
-@pd.api.extensions.register_dataframe_accessor("hbs")
-class HBSDF:
-    """Household Budget Survay pandas API"""
-
-    def __init__(self, data_table):
-        self.table: pd.DataFrame = data_table
-        self.year: int | None = None
-        self.id_column_name: str = "ID"
-        self.year_column_name: str = "Year"
-        self.code_column_name: str = "Code"
-
-    def add_attribute(self, attribute: _Attributes | list[_Attributes]) -> pd.DataFrame:
-        """
-        Add Household attributes to the DataFrame
-        """
-        return add_attribute(
-            self.table,
-            attribute=attribute,
-            attribute_text="names",
-            year=self.year,
-            id_column_name=self.id_column_name,
-            year_column_name=self.year_column_name,
-        )
-
-    def add_classification(
-        self,
-        classification: str = "original",
-        level: int | list[int] | None = None,
-        new_column_name: str | list[str] | None = None,
-    ) -> pd.DataFrame:
-        """
-        Add Commodities Classification to the DataFrame
-        """
-        return add_classification(
-            self.table,
-            classification=classification,
-            level=level,
-            new_column_name=new_column_name,
-            code_column_name=self.code_column_name,
-            year_column_name=self.year_column_name,
-        )
+            settings = decoder.CommodityDecoderSettings(name=value)
+            return decoder.CommodityDecoder(self._obj, settings).add_classification()
+        raise KeyError(f"{value} is not a view of the current table")
