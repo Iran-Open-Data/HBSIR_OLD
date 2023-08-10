b7ce59a1112d2560a405adb67358b933af319cde
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-08-10 16:10:41 +0330

    Update pandas api

--- hbsir/hbsframe.py
+++ hbsir/hbsframe.py
@@ -1,6 +1,8 @@
 """
 DataFrame extention
 """
+from importlib import import_module
+
 import pandas as pd
 
 from .metadata_reader import Attribute as _Attributes
@@ -28,6 +30,24 @@ class ViewAccessor:
 
     def __getitem__(self, value: str) -> pd.DataFrame:
         if self._views is not None and value in self._views:
-            settings = decoder.CommodityDecoderSettings(name=value)
+            settings = decoder.CommodityDecoderSettings(classification_name=value)
             return decoder.CommodityDecoder(self._obj, settings).add_classification()
         raise KeyError(f"{value} is not a view of the current table")
+
+
+@pd.api.extensions.register_dataframe_accessor("hbsir")
+class HBSIRFunctions:
+    def __init__(self, pandas_obj: pd.DataFrame) -> None:
+        self._obj = pandas_obj
+
+    def add_classification(self, **kwargs):
+        func = getattr(import_module("hbsir"), "add_classification")
+        return func(self._obj, **kwargs)
+
+    def add_attribute(self, **kwargs):
+        func = getattr(import_module("hbsir"), "add_attribute")
+        return func(self._obj, **kwargs)
+
+    def add_weight(self, **kwargs):
+        func = getattr(import_module("hbsir"), "add_weight")
+        return func(self._obj, **kwargs)
