72b214a7c5a190590f4cdc32a264e4158205a91a
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-08-11 01:35:59 +0330

    Remove unnecessary import

--- hbsir/data_cleaner.py
+++ hbsir/data_cleaner.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 
-from . import metadata_reader, utils
+from . import utils
 
 from .metadata_reader import (
     OriginalTable as _OriginalTable,
