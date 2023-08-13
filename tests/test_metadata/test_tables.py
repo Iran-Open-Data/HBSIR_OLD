418ae014506ab955ca194eb1233bf24226e3c355
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-08-13 13:15:08 +0330

    Add metadata test functions

---
+++ tests/test_metadata/test_tables.py
@@ -0,0 +1,24 @@
+import pytest
+
+import hbsir
+
+metadata = hbsir.metadata_reader.metadata
+defaults = hbsir.metadata_reader.defaults
+
+default_end = defaults.last_year + 1
+tables_availability = metadata.tables["yearly_table_availability"]
+
+def get_available_years(table_name: str):
+    table_availability = tables_availability[table_name]
+    years = hbsir.utils.Argham(table_availability, default_end=default_end).get_numbers()
+    years = list(years)
+    years.sort()
+    return years
+
+def build_table_year_pairs(table_name: str):
+    available_years = get_available_years(table_name)
+    return [(table_name, year) for year in available_years]
+
+@pytest.mark.parametrize("table_name,year", build_table_year_pairs("food"))
+def test_load_table(table_name, year):
+    hbsir.load_table(table_name, year, on_missing="create", recreate=True, save_created=False)
