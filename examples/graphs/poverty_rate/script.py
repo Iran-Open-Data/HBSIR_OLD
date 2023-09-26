f067485f06c7385ce1860ca59f910880d8e52b9d
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-09-26 18:48:05 +0330

    Format

--- examples/graphs/poverty_rate/script.py
+++ examples/graphs/poverty_rate/script.py
@@ -18,6 +18,7 @@ def add_fonts():
     except FileNotFoundError:
         pass
 
+
 add_fonts()
 
 style.use("fivethirtyeight")
@@ -31,15 +32,17 @@ plot_data = (
     .pipe(hbsir.add_attribute, "Urban_Rural")
     .query("Urban_Rural=='Urban'")
     .pipe(hbsir.add_classification, "Food-NonFood")
-    .groupby(["Year", "ID", "Food-NonFood"])["Gross_Expenditure"].sum()
+    .groupby(["Year", "ID", "Food-NonFood"])["Gross_Expenditure"]
+    .sum()
     .unstack()
-    .rename(columns = {"Non-Food": "NonFood"})
-    .assign(Ratio=lambda df : df.eval("Food / (Food + NonFood) * 100"))
+    .rename(columns={"Non-Food": "NonFood"})
+    .assign(Ratio=lambda df: df.eval("Food / (Food + NonFood) * 100"))
     .dropna(subset="Ratio")
     .pipe(hbsir.add_weight)
-    .assign(Poor=lambda df : df.eval("(Ratio >= 40) * Weight"))
+    .assign(Poor=lambda df: df.eval("(Ratio >= 40) * Weight"))
     .pipe(hbsir.add_attribute, "Province")
-    .groupby(["Year", "Province"])[["Poor", "Weight"]].sum()
+    .groupby(["Year", "Province"])[["Poor", "Weight"]]
+    .sum()
     .eval("Poor / Weight * 100")
     .unstack(0)
     .rename(columns={FIRST_YEAR: str(FIRST_YEAR), LAST_YEAR: str(LAST_YEAR)})
@@ -57,36 +60,30 @@ ax.scatter(plot_data[str(LAST_YEAR)], plot_data.index, label=LAST_YEAR)
 for i, row in plot_data.iterrows():
     if row["diff"] >= 3:
         ax.annotate(
-            text = "",
-            xy = (
+            text="",
+            xy=(
                 row[str(LAST_YEAR)] + 0.5,
                 i,
             ),
-            xytext = (
+            xytext=(
                 row[str(FIRST_YEAR)] - 0.6,
                 i,
             ),
-            arrowprops = dict(
-                color = "green",
-                arrowstyle = "-|>"
-            )
+            arrowprops=dict(color="green", arrowstyle="-|>"),
         )
 
     if row["diff"] <= -3:
         ax.annotate(
-            text = "",
-            xy = (
+            text="",
+            xy=(
                 row[str(LAST_YEAR)] - 0.5,
                 i,
             ),
-            xytext = (
+            xytext=(
                 row[str(FIRST_YEAR)] + 0.6,
                 i,
             ),
-            arrowprops = dict(
-                color = "red",
-                arrowstyle = "-|>"
-            )
+            arrowprops=dict(color="red", arrowstyle="-|>"),
         )
 
 fig.suptitle(f"Provincial Poverty* Rate Changes {FIRST_YEAR} - {LAST_YEAR}")
@@ -97,7 +94,7 @@ fig.text(
     0.06,
     0.02,
     r"*Poor: A household whose share of food, exceeds 40% of its total expenditures.",
-    fontdict={"size": 10}
+    fontdict={"size": 10},
 )
 
 fig.subplots_adjust(left=0.35, bottom=0.12, right=0.98, top=0.92)
