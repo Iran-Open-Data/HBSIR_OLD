198b086c75fd52c12171ae6cd578ab2483f587ae
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-10-05 08:25:01 +0330

    Remove outdated tests

--- tests/test_package/test_package_structure.py
+++
@@ -1,7 +0,0 @@
-from hbsir import archive_handler, data_cleaner, data_engine, metadata_reader
-import pytest
-
-
-def test_zero_division():
-    with pytest.raises(ZeroDivisionError):
-        1 / 0
