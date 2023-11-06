50cbf3f9bbf732f239cd16df02358325deb24d94
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-11-07 01:52:51 +0330

    Refactor maintainer to make it multi-thread

--- hbsir/maintainer.py
+++ hbsir/maintainer.py
@@ -1,13 +1,14 @@
 """For maintainer"""
-
+import concurrent.futures
 from pathlib import Path
 import tomllib
-from typing import Iterable
+from typing import Iterable, Literal
 
 import requests
 from tqdm import tqdm
 import boto3
 
+from .api import setup
 from .core.metadata_reader import (
     defaults,
     _OriginalTable,
@@ -16,9 +17,11 @@ from .core.metadata_reader import (
 from . import utils
 
 
-def update_online_files() -> None:
+def update_online_files(recreate=False, replace=False) -> None:
+    if recreate:
+        setup(years="all", method="create", table_names="all", replace=replace)
     for directories in [
-        # (defaults.processed_data, "parquet_files"),
+        (defaults.processed_data, "parquet_files"),
         (defaults.external_data, "external_data"),
     ]:
         update_online_directory(*directories)
@@ -27,36 +30,40 @@ def update_online_files() -> None:
 def update_online_directory(local_directory: Path, online_directory: str) -> None:
     file_stats = _scan_directory(local_directory, online_directory)
     to_up_files = [file for file, stat in file_stats.items() if stat != "UpToDate"]
-    pbar = tqdm(total=len(to_up_files), desc="Preparing ...", unit="Table")
-    for file in to_up_files:
-        pbar.update()
-        pbar.desc = f"Checking file: {file}"
-        _publish_parquet_file(local_directory.joinpath(file), online_directory)
-    pbar.close()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+        for file in to_up_files:
+            executor.submit(
+                _publish_parquet_file, local_directory.joinpath(file), online_directory
+            )
 
 
-def _scan_directory(local_directory: Path, online_directory: str):
-    file_stats = {}
-    number_of_files = len(
-        [file for file in local_directory.glob("*") if file.is_file()]
-    )
-    pbar = tqdm(total=number_of_files, desc="Preparing ...", unit="Table")
-    for path in local_directory.iterdir():
-        pbar.update()
-        pbar.desc = f"Checking file: {path.name}"
-        if path.is_dir():
-            continue
-        online_file_size = _get_file_size_online_directory(path.name, online_directory)
-        local_size = path.stat().st_size
-
-        if online_file_size is None:
-            file_stats[path.name] = "Missing"
-        elif abs(online_file_size - local_size) < 5:
-            file_stats[path.name] = "UpToDate"
-        else:
-            file_stats[path.name] = "OutDated"
-    pbar.close()
+def _scan_directory(local_directory: Path, online_directory: str) -> dict:
+    futures_dict = {}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+        for path in local_directory.iterdir():
+            if path.is_dir():
+                continue
+            futures_dict.update(
+                {executor.submit(_get_file_stats, path, online_directory): path}
+            )
+    file_stats = {
+        futures_dict[future].name: future.result()
+        for future in concurrent.futures.as_completed(futures_dict)
+    }
     return file_stats
+
+
+def _get_file_stats(
+    path: Path, online_directory: str
+) -> Literal["Missing", "UpToDate", "OutDated"]:
+    online_file_size = _get_file_size_online_directory(path.name, online_directory)
+    local_size = path.stat().st_size
+
+    if online_file_size is None:
+        return "Missing"
+    if abs(online_file_size - local_size) < 5:
+        return "UpToDate"
+    return "OutDated"
 
 
 def _get_file_size_online_directory(file_name: str, directory):
